c01cf318148f51a5d496709799f4c4e40cc31e39
Roman Kalachik <roman.kalachik@devexpress.com>
2020-09-22 16:20:37 +0300

    first commit

---
+++ WinCharts/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WinCharts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.layoutConverter1 = new DevExpress.XtraLayout.Converter.LayoutConverter(this.components);
+            this.Form1layoutControl1ConvertedLayout = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.chartControl1item = new DevExpress.XtraLayout.LayoutControlItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Form1layoutControl1ConvertedLayout)).BeginInit();
+            this.Form1layoutControl1ConvertedLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1item)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.Legend.Name = "Default Legend";
+            this.chartControl1.Location = new System.Drawing.Point(158, 12);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControl1.Size = new System.Drawing.Size(630, 440);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // Form1layoutControl1ConvertedLayout
+            // 
+            this.Form1layoutControl1ConvertedLayout.Controls.Add(this.button1);
+            this.Form1layoutControl1ConvertedLayout.Controls.Add(this.chartControl1);
+            this.Form1layoutControl1ConvertedLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Form1layoutControl1ConvertedLayout.Location = new System.Drawing.Point(0, 0);
+            this.Form1layoutControl1ConvertedLayout.Name = "Form1layoutControl1ConvertedLayout";
+            this.Form1layoutControl1ConvertedLayout.Root = this.layoutControlGroup1;
+            this.Form1layoutControl1ConvertedLayout.Size = new System.Drawing.Size(800, 464);
+            this.Form1layoutControl1ConvertedLayout.TabIndex = 4;
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.chartControl1item,
+            this.layoutControlItem1,
+            this.emptySpaceItem1});
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(800, 464);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // chartControl1item
+            // 
+            this.chartControl1item.Control = this.chartControl1;
+            this.chartControl1item.Location = new System.Drawing.Point(146, 0);
+            this.chartControl1item.Name = "chartControl1item";
+            this.chartControl1item.Size = new System.Drawing.Size(634, 444);
+            this.chartControl1item.TextSize = new System.Drawing.Size(0, 0);
+            this.chartControl1item.TextVisible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 20);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.button1;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(146, 24);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 24);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(146, 420);
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 464);
+            this.Controls.Add(this.Form1layoutControl1ConvertedLayout);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Form1layoutControl1ConvertedLayout)).EndInit();
+            this.Form1layoutControl1ConvertedLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1item)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private DevExpress.XtraLayout.Converter.LayoutConverter layoutConverter1;
+        private DevExpress.XtraLayout.LayoutControl Form1layoutControl1ConvertedLayout;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem chartControl1item;
+        private System.Windows.Forms.Button button1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+    }
+}
+
---
+++ WinCharts/Form1.cs
@@ -0,0 +1,210 @@
+﻿using DataGenerator;
+using DevExpress.XtraCharts;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Windows;
+using System.Windows.Forms;
+namespace WinCharts
+{
+    public partial class Form1 : Form
+    {
+        ObservableCollection<DataItem> chartSource;
+        string log;
+        long prevAvailable = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void bindData(bool allowResample)
+        {
+            Series series = new Series();
+            chartControl1.Series.Add(series);
+            series.View = new LineSeriesView();
+            series.AllowResample = allowResample;
+            series.BindToData(chartSource, "Argument", "Value");
+            XYDiagram2D d2d = chartControl1.Diagram as XYDiagram2D;
+            if (d2d != null)
+            {
+                d2d.ZoomingOptions.AxisXMaxZoomPercent = 100000000;
+                d2d.ZoomingOptions.AxisYMaxZoomPercent = 100000000;
+            }
+        }
+        void button1_Click(object sender, EventArgs e)
+        {
+            MeasureAll(null, null);
+        }
+        void ClearChart()
+        {
+            DoEvents();
+            Series series = chartControl1.Series.FirstOrDefault() as Series;
+            if (series != null) series.DataSource = null;
+            XYDiagram d2d = chartControl1.Diagram as XYDiagram;
+            d2d?.ResetZoom();
+            chartControl1.Series.Clear();
+
+        }
+        void DoEvents()
+        {
+            System.Windows.Forms.Application.DoEvents();
+        }
+        void LoadData(int pointsCount, bool allowResample)
+        {
+            ClearChart();
+            chartSource = Generator.Generate(pointsCount);
+            LogMemConsumption();
+            bindData(allowResample);
+            DoEvents();
+        }
+        void LoadDataEx(int pointsCount, bool allowResample)
+        {
+            chartSource = Generator.Generate(pointsCount);
+            bindData(allowResample);
+            DoEvents();
+        }
+        long LogMemConsumption()
+        {
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            GC.Collect(2, GCCollectionMode.Forced);
+            GC.WaitForFullGCComplete();
+            long available = GC.GetTotalMemory(true);
+
+            long delta = prevAvailable - available;
+            prevAvailable = available;
+            return delta;
+        }
+        void MeasureAll(object sender, RoutedEventArgs e)
+        {
+            MeasureLoading(null, null);
+            Text = "Loading - done";
+            MeasureMemConsumption(null, null);
+            Text = "Memory - done";
+            MeasureZoom(null, null);
+            Text = "Zooming - done";
+            MeasurePan(null, null);
+            Text = "All - done";
+        }
+        void MeasureLoading(object sender, RoutedEventArgs e)
+        {
+            string result = string.Empty;
+            Iterate("Loading", new Action<int>(count =>
+            {
+                ClearChart();
+                Stopwatch resampledLoading = new Stopwatch();
+                Stopwatch nonResampledLoading = new Stopwatch();
+                resampledLoading.Start();
+                LoadDataEx(count, true);
+                resampledLoading.Stop();
+                ClearChart();
+                nonResampledLoading.Start();
+                LoadDataEx(count, false);
+                nonResampledLoading.Stop();
+                result += string.Format("{0}, {1}, {2}{3}", count, resampledLoading.ElapsedMilliseconds, nonResampledLoading.ElapsedMilliseconds, Environment.NewLine);
+            }));
+            File.WriteAllText("result_loading.txt", result);
+
+
+        }
+        void MeasureMemConsumption(object sender, RoutedEventArgs e)
+        {
+            ClearChart();
+            MeasureMemConsumptionCore(true);
+            ClearChart();
+            MeasureMemConsumptionCore(false);
+        }
+        void MeasureMemConsumptionCore(bool allowResampling)
+        {
+            string result = string.Empty;
+            Iterate("MemConsumption", new Action<int>(count =>
+            {
+                LoadData(count, allowResampling);
+                long chartSize = LogMemConsumption();
+                result += string.Format("{0}, {1}{2}", count, -chartSize, Environment.NewLine);
+            }));
+            File.WriteAllText(string.Format("result_memconsumptoin_{0}.txt", allowResampling), result);
+        }
+        void MeasurePan(object sender, RoutedEventArgs e)
+        {
+            string result = string.Empty;
+            Iterate("Pan", new Action<int>(count =>
+            {
+                Stopwatch resampled = new Stopwatch();
+                Stopwatch nonResampled = new Stopwatch();
+                ClearChart();
+                LoadDataEx(count, true);
+                XYDiagram2D xyd2d = Chart.Diagram as XYDiagram2D;
+                xyd2d.EnableAxisXZooming = true;
+                xyd2d.EnableAxisYZooming = true;
+                xyd2d.ZoomIn(null);
+                resampled.Start();
+                PerofrmScroll(xyd2d);
+                resampled.Stop();
+                ClearChart();
+                LoadDataEx(count, false);
+                xyd2d = Chart.Diagram as XYDiagram2D;
+                nonResampled.Start();
+                PerofrmScroll(xyd2d);
+                nonResampled.Stop();
+                result += string.Format("{0}, {1}, {2}{3}", count, resampled.ElapsedMilliseconds, nonResampled.ElapsedMilliseconds, Environment.NewLine);
+            }));
+            File.WriteAllText("result_scroll.txt", result);
+
+        }
+        void MeasureZoom(object sender, RoutedEventArgs e)
+        {
+            string result = string.Empty;
+            Iterate("Zoom", new Action<int>(count =>
+            {
+                ClearChart();
+                Stopwatch resampled = new Stopwatch();
+                Stopwatch nonResampled = new Stopwatch();
+                LoadDataEx(count, true);
+                XYDiagram2D xyd2d = Chart.Diagram as XYDiagram2D;
+                resampled.Start();
+                PerformZoom(xyd2d);
+                resampled.Stop();
+                ClearChart();
+                LoadDataEx(count, false);
+                xyd2d = Chart.Diagram as XYDiagram2D;
+                nonResampled.Start();
+                PerformZoom(xyd2d);
+                nonResampled.Stop();
+                result += string.Format("{0}, {1}, {2}{3}", count, resampled.ElapsedMilliseconds, nonResampled.ElapsedMilliseconds, Environment.NewLine);
+            }));
+            File.WriteAllText("result_zoom.txt", result);
+        }
+        void PerformZoom(XYDiagram2D xyd2d)
+        {
+            xyd2d.EnableAxisXZooming = true;
+            xyd2d.EnableAxisYZooming = true;
+            for (int i = 0; i < 5; i++)
+            {
+                xyd2d.ZoomIn(null);
+                DoEvents();
+            }
+        }
+        void PerofrmScroll(XYDiagram2D xyd2d)
+        {
+            xyd2d.EnableAxisXScrolling = true;
+            xyd2d.EnableAxisYScrolling = true;
+            for (int i = 0; i < 25; i++)
+            {
+                DoEvents();
+            }
+        }
+        protected void Iterate(string caption, Action<int> action)
+        {
+            int[] counts = new int[] { 1000, 10000, 20000, 50000, 100000, 300000, 500000, 750000, 1000000, 2000000 };
+            int counter = 0;
+            foreach (int count in counts)
+            {
+                Text = caption + " " + counter++.ToString() + " of " + counts.Length;
+                action(count);
+            }
+        }
+        public ChartControl Chart { get { return chartControl1; } }
+    }
+}
